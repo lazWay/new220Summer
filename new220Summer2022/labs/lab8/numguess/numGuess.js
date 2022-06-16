509c6f7b8f3a428c21b69f2670323c250e1bcc40
Ronald Hartke <Bottomlines918@gmail.com>
2022-06-15 21:51:40 -0400

    User input exercises complete

--- new220Summer2022/labs/lab8/numguess/numGuess.js
+++ new220Summer2022/labs/lab8/numguess/numGuess.js
@@ -7,18 +7,18 @@ function guess() {
     let numGuess = Number(txtGuess.value); // converts txtGuess.value (string) into a numeric value.
     let randomNum = 1 + Math.floor(Math.random() * numGuess); // results a random number 0:20.
    
-    dvResult.innerHTML = randomNum; // outputs txt, in this case, the result of randomNum.
-    randomNum.value = ""; // clearing out value
-
-        if (txtGuess.value == 7){
-        dvResult.innerHTML = "WIN!"
-        if (txtGuess.value > 7)
-        dvResult.innerHTML = "lower.."
-        if (txtGuess.value < 7){
+    //dvResult.innerHTML = randomNum; // outputs txt, in this case: the result of randomNum.
+   
+        if (numGuess == 7){
+            dvResult.innerHTML = "WIN!"
+        }
+        if (numGuess > 7) {
+            dvResult.innerHTML = "lower.."
+        }
+        if (numGuess < 7) {
             dvResult.innerHTML = "higher ... "
-        } else {dvResult.innerHTML = randomNum}
-      
-            
+        }
+         else {
+            txtGuess.value =  ""; // clearing out value  
     }
 }
-
--- new220Summer2022/labs/lab8/passwordProtected/passwordProtected.js
+++ new220Summer2022/labs/lab8/passwordProtected/passwordProtected.js
@@ -1,1 +1,16 @@
-prompt ("Enter a UserName ");
+let userName = document.querySelector("#txtName"); // assigns userName to an input field on the userend.
+let dvOutput = document.querySelector("#dvOutput"); // assings a response to the user's input. 
+let password = document.querySelector("#txtPassword"); // assigns a second txt entry password** for future relation to the userName and password sequence.
+
+//fn to create a username and password input field. 
+function access(){
+    console.log (userName) ;
+    dvOutput.innerHTML = "" + password;
+console.log (userName,password)
+
+    if ((userName.value == "Ron") && (password.value == "Ronnie")){ // makes a relation to two objects and the correct series of string inputs.
+    dvOutput.innerHTML = "Success" 
+    } else {
+        dvOutput.innerHTML = "Wrong information " + userName.value; // prompts user that input is wrong.
+    }
+}
