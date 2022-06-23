b0b217320915089400382aebe34bce2d0bac8fef
Ronald Hartke <Bottomlines918@gmail.com>
2022-06-23 13:37:00 -0400

    Final Commit - Thanks prof.Faus
    Final Submission NEWM-220

--- new220Summer2022/labs/lab11/flashCards/flashCards.js
+++ new220Summer2022/labs/lab11/flashCards/flashCards.js
@@ -1,8 +1,7 @@
-//REWORK
 
 function myFunction(clicked) {
     if (clicked=="Indianapolis")//here it check if value is correct
-    document.getElementById("answer").innerHTML = "It is Right Answer ::"+clicked; //message to div for user
+    document.getElementById("answer").innerHTML = ""+clicked; //message to div for user
     else
     document.getElementById("answer").innerHTML = "You have Choose Incorrect.True Answer is ::Indianapolis"; //message to div for user
 }
